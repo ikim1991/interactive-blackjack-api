c9eed565147038ddb5bd0eeb5a4624a22bbb75d9
Chris K <ikim1991@gmail.com>
2020-07-02 14:43:17 -0400

    Setting up server and Websocket for Chat Functionality

--- src/server.js
+++ src/server.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+const router = require('./router');
+const generateMessage = require('./utils/generateMessage')
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.listen(PORT, () => {
-  console.log(`Running on PORT ${PORT}...`)
+io.on('connection', (socket) => {
+  console.log("Client Connected...");
+
+  socket.on("sendMessage", ({ user, message}) => {
+    io.emit("message", generateMessage(user, message))
+  })
+
+
+  socket.on('disconnect', () => {
+    console.log("User has left...")
+  })
+
+})
+
+app.use(router);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`)
 })
---
+++ src/utils/generateMessage.js
@@ -0,0 +1,9 @@
+const generateMessage = (user, text) => {
+  return {
+    username: user.username,
+    text: text,
+    createdAt: new Date().getTime()
+  }
+}
+
+module.exports = generateMessage
